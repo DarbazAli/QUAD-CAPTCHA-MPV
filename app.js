bb985d1f17f4d58d8add2f2cb5b56047b56c035d
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-19 13:23:10 +0300

    implement CAPTCHA

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const bodyParser = require('body-parser');
 const express = require('express')
 
 const app = express();
+console.clear();
 
 app.listen(3000, () => console.log("Listening on 3000"))
 
@@ -9,13 +11,31 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 // serve static files
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // create the home url
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home', message: "Hello There"})
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+app
+    .route('/register')
+    .post( (req, res) => {
+        const {email, quad_captcha} = req.body;
+        // console.log(email, quad)
+        if ( quad_captcha ) {
+            console.log('thankyou', email, quad_captcha)
+        }
+        res.redirect('/')
+    })
+
+app
+    .route('/thank')
+    .get((req, res) => {
+        // const { check, email } = req.body;
+        res.send(`Hello, it looks like you passed the cpatcha`)
+    })
